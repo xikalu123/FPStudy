466e08eff4ac2a0a7addf911cdf07041f18d8914
chenyuliang <chenyuliang@didichuxing.com>
2019-02-26 20:17:18 +0800

    complete recursion

--- recursion.hs
+++ recursion.hs
@@ -26,3 +26,24 @@ take' n (x:xs) = x : take' (n-1) xs
 reverse' :: [a]->[a]
 reverse' [] = []
 reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a->[a]
+repeat' x = x:repeat' x
+
+zip' :: [a]->[b]->[(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+elem' :: (Eq a) => a->[a]->Bool
+elem' a [] = False
+elem' a (x:xs)
+   | a == x     = True
+   | otherwise  = a `elem'` xs
+
+quicksort :: (Ord a) => [a]->[a]
+quicksort [] = []
+quicksort (x:xs) = 
+    let smallsort = quicksort [a | a <- xs, a < x]
+        largesort = quicksort [a | a <- xs, a > x]
+    in smallsort ++ [x] ++ largesort
