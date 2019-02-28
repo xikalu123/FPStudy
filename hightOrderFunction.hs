b04c4913d20cbf53ac594752c5568e295d6df336
chenyuliang <chenyuliang@didichuxing.com>
2019-02-28 19:44:33 +0800

    highOrderFunction 3

--- hightOrderFunction.hs
+++ hightOrderFunction.hs
@@ -22,3 +22,34 @@ zipWith' :: (a->b->c)->[a]->[b]->[c]
 zipWith' _ [] _ = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a->b->c)->b->a->c
+flip' f x y = f y x
+
+map' :: (a->b)->[a]->[b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a->Bool)->[a]->[a]
+filter' _ [] = []
+filter' p (x:xs)
+     | p x   = x : filter' p xs
+     | otherwise = filter' p xs
+
+quicksort :: (Ord a) => [a]->[a]
+quicksort [] = []
+quicksort (x:xs) = 
+    let smallsort = quicksort (filter' (<= x) xs)
+        largesort = quicksort (filter' (> x) xs)
+    in smallsort ++ [x] ++ largesort
+
+largestDivisible :: (Integral a) => a 
+largestDivisible = head (filter' p [100000,99999..])
+      where p x = x `mod` 3828 == 0
+
+chain :: (Integral a) => a->[a]
+chain 1 = [1]
+chain n
+    | even n = n : chain(n `div` 2)
+    | odd n = n : chain(n * 3 + 1)
+  
