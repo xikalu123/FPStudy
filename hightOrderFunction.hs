bb9f89a199c42b7ad229c5fefadc2d6ec2f5c91d
chenyuliang <chenyuliang@didichuxing.com>
2019-03-04 20:02:31 +0800

    high 4

--- hightOrderFunction.hs
+++ hightOrderFunction.hs
@@ -63,3 +63,38 @@ numLongChains' = length (filter (\xs->length xs > 15) (map chain [1..100]))
 
 flip'' :: (a->b->c)->b->a->c
 flip'' f = \x y -> f y x
+
+-- foldl 应用
+-- sum' :: (Num a) => [a]->a
+-- sum' xs = foldl (\acc x -> acc + x) 0 xs
+
+sum' :: (Num a) => [a]->a
+sum' = foldl (*) 1
+
+elem' :: (Eq a) =>a->[a]-> Bool
+elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+map'' :: (a->b) ->[a]->[b]
+map'' f xs = foldr (\x acc -> f x : acc) [] xs
+
+maximum' :: (Ord a) => [a]->a
+maximum' = foldl1 (\acc x -> if x > acc then x else acc)
+
+-- reverse' :: [a] -> [a]
+-- reverse' = foldl (\acc x -> x : acc) []
+
+reverse' :: [a] -> [a]
+reverse' = foldl (flip(:)) []
+
+product' :: (Num a) => [a]->a
+product' = foldl1(*)
+
+filter'' :: (a->Bool) -> [a] ->[a]
+filter'' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+head' :: [a]->a
+head' = foldr1 (\x _ -> x)
+
+last' :: [a]->a
+last' = foldl1 (\_ x -> x)
+
