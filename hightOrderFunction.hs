25c5bb46389b2201cf213120f9b1f6d38e9a6654
chenyuliang <chenyuliang@didichuxing.com>
2019-03-05 11:01:20 +0800

    highOrder

--- hightOrderFunction.hs
+++ hightOrderFunction.hs
@@ -98,3 +98,9 @@ head' = foldr1 (\x _ -> x)
 last' :: [a]->a
 last' = foldl1 (\_ x -> x)
 
+oddSquareSum :: Integer
+oddSquareSum = 
+    let oddSquares = filter odd $ map (^2) [1..]
+        belowLimit = takeWhile (<10000) oddSquares
+    in sum belowLimit
+
